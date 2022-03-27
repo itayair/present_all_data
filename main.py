85095f20bff407adb394309534370cee771b279e
itayair <iy24592@gmail.com>
2022-03-27 15:28:52 +0300

    add project

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ import csv
 import spacy
 import pip
 
-def install(package):
-    if hasattr(pip, 'main'):
-        pip.main(['install', package])
-    else:
-        pip._internal.main(['install', package])
-
-# Example
-if __name__ == '__main__':
-    install("https://storage.googleapis.com/en_ud_model/en_ud_model_sm-2.0.0.tar.gz")
+# def install(package):
+#     if hasattr(pip, 'main'):
+#         pip.main(['install', package])
+#     else:
+#         pip._internal.main(['install', package])
+#
+# # Example
+# if __name__ == '__main__':
+#     install("https://storage.googleapis.com/en_ud_model/en_ud_model_sm-2.0.0.tar.gz")
 
 class obj_to_json:
     def __init__(self, label, value):
